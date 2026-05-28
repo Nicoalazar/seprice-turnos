bbdecaa4da72e2cfe6b28610d75ff7d9a0e89de8
nico <nicozalazar67@gmail.com>
2026-05-28 09:02:23 -0300

    FIx: implementar lazy loading en routes y modificación de budgets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,44 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SobreturnoComponent } from './pages/turnos/sobreturno/sobreturno.component';
-import { CancelarTurnoComponent } from './pages/turnos/cancelar-turno/cancelar-turno.component';
-import { AgendaComponent } from './pages/turnos/agenda/agenda.component'; 
 
 export const routes: Routes = [
-    {path: 'login', component: LoginComponent},
-    {path: 'dashboard', component: DashboardComponent},
-    {path: 'sobreturnos', component: SobreturnoComponent},
-    {path: 'cancelar-turno', component: CancelarTurnoComponent}, 
-    {path: 'agenda', component: AgendaComponent}, 
-    {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./auth/login/login.component')
+        .then(m => m.LoginComponent)
+  },
+
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard.component')
+        .then(m => m.DashboardComponent)
+  },
+
+  {
+    path: 'sobreturnos',
+    loadComponent: () =>
+      import('./pages/turnos/sobreturno/sobreturno.component')
+        .then(m => m.SobreturnoComponent)
+  },
+
+  {
+    path: 'cancelar-turno',
+    loadComponent: () =>
+      import('./pages/turnos/cancelar-turno/cancelar-turno.component')
+        .then(m => m.CancelarTurnoComponent)
+  },
+
+  {
+    path: 'agenda',
+    loadComponent: () =>
+      import('./pages/turnos/agenda/agenda.component')
+        .then(m => m.AgendaComponent)
+  },
+
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  }
 ];
